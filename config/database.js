44a19e3ccb68d64c0551d75c12e530ae6fcfaebd
Santiago Semhan <santiagosemhan@gmail.com>
2020-12-14 17:41:20 -0300

    Initial strapi configuration

---
+++ config/database.js
@@ -0,0 +1,35 @@
+module.exports = ({ env }) => {
+  let defaultConnection = {
+    connector: "mongoose",
+    settings: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      srv: env.bool("DATABASE_SRV", false),
+      port: env.int("DATABASE_PORT", 27017),
+      database: env("DATABASE_NAME", "misiones-travel"),
+      username: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "root"),
+    },
+    options: {
+      authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+  }
+  if (env('MONGO_DSN')) {
+    defaultConnection = {
+      connector: 'mongoose',
+      settings: {
+        uri: env('MONGO_DSN', ''),
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    }
+  }
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: defaultConnection,
+    },
+  };
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '321a04bc-58e7-4354-8812-622bdac5b6e6'
+};
