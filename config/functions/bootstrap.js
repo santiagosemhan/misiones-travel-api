4321edc4af70c46ac64dfb1a4152dbc0c948a389
Santiago Semhan <santiagosemhan@gmail.com>
2020-12-17 17:59:44 -0300

    Initial Model  v1.

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,5 @@
 'use strict';
+const axios = require('axios');
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +11,13 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+  const { data } = await axios.get('https://apis.datos.gob.ar/georef/api/localidades?provincia=54&campos=localidad_censal,centroide&max=200');
+
+  const { localidades } = data;
+  const loadedLocalidades = await strapi.services.localidad.find();
+  if (loadedLocalidades.length === 0) {
+    await Promise.all(localidades.map(l => strapi.services.localidad.create({ nombre: l.nombre, latitud: l.centroide.lat, longitud: l.centroide.lon })));
+  }
+  console.log(data);
+};
