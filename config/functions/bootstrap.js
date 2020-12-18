40ef1e557fa08e2f17d82e9d5ad542598a7b766d
Santiago Semhan <santiagosemhan@gmail.com>
2020-12-18 08:55:28 -0300

    Remove initial import of localidades

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -11,13 +11,4 @@ const axios = require('axios');
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = async () => {
-  const { data } = await axios.get('https://apis.datos.gob.ar/georef/api/localidades?provincia=54&campos=localidad_censal,centroide&max=200');
-
-  const { localidades } = data;
-  const loadedLocalidades = await strapi.services.localidad.find();
-  if (loadedLocalidades.length === 0) {
-    await Promise.all(localidades.map(l => strapi.services.localidad.create({ nombre: l.nombre, latitud: l.centroide.lat, longitud: l.centroide.lon })));
-  }
-  console.log(data);
-};
+module.exports = () => {};
