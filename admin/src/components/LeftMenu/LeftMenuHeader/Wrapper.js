515c5ee61059f65c2325431e415b9593153b8f26
Santiago Semhan <santiagosemhan@gmail.com>
2020-12-29 16:23:32 -0300

    Customize admin

---
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Logo from '../../../assets/images/logo-strapi.png';
+
+const Wrapper = styled.div`
+
+  background-color: #fff;
+  box-shadow: 0 1px 2px 0 rgba(40,42,49,0.16)
+  height: ${props => props.theme.main.sizes.leftMenu.height};
+
+  .leftMenuHeaderLink {
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  .projectName {
+    display: block;
+    width: 100%;
+    height: ${props => props.theme.main.sizes.leftMenu.height};
+    font-size: 2rem;
+    letter-spacing: 0.2rem;
+    color: $white;
+
+    background-image: url(${Logo});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 155px;
+  }
+`;
+
+Wrapper.defaultProps = {
+  theme: {
+    main: {
+      colors: {
+        leftMenu: {},
+      },
+      sizes: {
+        header: {},
+        leftMenu: {},
+      },
+    },
+  },
+};
+
+Wrapper.propTypes = {
+  theme: PropTypes.object,
+};
+
+export default Wrapper;
