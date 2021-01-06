5fb8176d59eb3b5251b73cf8632d084e717c10da
Santiago Semhan <santiagosemhan@gmail.com>
2021-01-06 10:18:48 -0300

    Slugify categoria

--- api/categoria/models/categoria.js
+++ api/categoria/models/categoria.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const getSlug = async (categoria) => {
+  let str;
+  let slug;
+  if (categoria.nombre) {
+    str = categoria.nombre;
+    if (categoria.categoria) {
+      const parent = await strapi.services.categoria.findOne({ id: categoria.categoria });
+      str = `${parent.nombre} ${str}`;
+    }
+    slug = slugify(str, { lower: true });
+  }
+  return slug;
+};
+
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      data.slug = await getSlug(data);
+    },
+    async beforeUpdate(id, data) {
+      data.slug = await getSlug(data);
+    },
+  },
+};
