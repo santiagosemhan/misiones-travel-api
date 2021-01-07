c61b6ea6df17b9e8bfa6bea837b57104c12556df
Santiago Semhan <santiagosemhan@gmail.com>
2021-01-07 19:51:22 -0300

    Add location field to lugares

--- api/lugar/models/lugar.js
+++ api/lugar/models/lugar.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const getLocation = async (data) => {
+  let location;
+  if (
+    data.geoposicion &&
+    data.geoposicion.latitud &&
+    data.geoposicion.longitud
+  ) {
+    location = {
+      type: "Point",
+      coordinates: [data.geoposicion.latitud, data.geoposicion.longitud],
+    };
+  }
+  return location;
+};
+
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      data.location = await getLocation(data);
+    },
+    async beforeUpdate(id, data) {
+      data.location = await getLocation(data);
+    },
+  },
+};
