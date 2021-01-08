c03e97adcddf58fec7a6b5d3ecde36cd89180519
Santiago Semhan <santiagosemhan@gmail.com>
2021-01-08 10:59:58 -0300

    Add near endpoint

--- api/lugar/controllers/lugar.js
+++ api/lugar/controllers/lugar.js
@@ -1,8 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // GET /near
+  async near(ctx) {
+    const { query } = ctx.request;
+    const { coordinates, minDistance = 100, maxDistance = 5000 } = query;
+    if (!coordinates) {
+      ctx.status = 400;
+      return ctx.throw(400, "coordinates param is required.");
+    }
+    const [lat, lng] = coordinates.split(",").map((i) => Number(i));
+
+    if (!(lat && lng)) {
+      return ctx.throw(400, "coordinates param is invalid.");
+    }
+
+    const lugares = await strapi.query("lugar").model.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [lat, lng],
+          },
+          $maxDistance: maxDistance,
+          $minDistance: minDistance,
+        },
+      },
+      published_at: { $ne: null },
+    });
+
+    ctx.send(lugares);
+  },
+};
